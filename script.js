904bf7bb5ba8e182eb767f5bcdd84c8a0abb4a23
ADIL <adilshaik9112@gmail.com>
2026-02-20 08:42:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+const select = document.getElementById("colorSelect");
+const button = document.queryselector('input[type="button"]');
+
+button.addEventListener("click", function () {
+    // Get selected index
+    const selectedIndex = select.selectedIndex;
+
+    // Remove only if an option is selected
+    if (selectedIndex !== -1) {
+        select.remove(selectedIndex);
+    }
+});
